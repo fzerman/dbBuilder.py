246d137f6e9d5445bf2df3432689a12d4068aed0
Swindler36 <furkanzerman@gmail.com>
2021-02-16 12:17:10 +0300

    adding validator shortcuts

---
+++ db_builder/validators/__init__.py
@@ -0,0 +1,5 @@
+from .BigLetterValidator import BigLetterValidator
+from .EmailValidator import EmailValidator
+from .LengthValidator import LengthValidator
+from .SmallLetterValidator import SmallLetterValidator
+from .SpecialCharValidator import SpecialCharValidator
