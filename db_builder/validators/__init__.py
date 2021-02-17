53f22f4f792c739aee315a13f48327cfe41f3f09
Swindler36 <furkanzerman@gmail.com>
2021-02-17 23:46:02 +0300

    registering numbervalidator

--- db_builder/validators/__init__.py
+++ db_builder/validators/__init__.py
@@ -3,3 +3,4 @@ from .EmailValidator import EmailValidator
 from .LengthValidator import LengthValidator
 from .SmallLetterValidator import SmallLetterValidator
 from .SpecialCharValidator import SpecialCharValidator
+from .NumberValidator import NumberValidator
