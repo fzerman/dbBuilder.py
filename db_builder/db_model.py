e67ad48ff1d960a4a9f0d7d12dc9f0b8ce7a6f41
Swindler36 <furkanzerman@gmail.com>
2021-02-18 01:33:00 +0300

    integration of save, filter fixed bugs  of objects

--- db_builder/db_model.py
+++ db_builder/db_model.py
@@ -8,6 +8,11 @@ class DB_Model():
         self.model_name = self.__class__.__name__
         self.instance_dict = self.__class__.__dict__
         self.query_set = []
+        self.kwargs = kwargs
+
+        self.set_kwargs_as_attr()        
+
+    def set_kwargs_as_attr(self):
         # IDField
         id_field = IDField()
         setattr(self,"id_field",DB_FieldObject(self.__class__,id_field))
@@ -18,17 +23,21 @@ class DB_Model():
         for i in self.instance_dict:
             if not i.startswith("__"):
                 setattr(self,i+"_field",DB_FieldObject(self.__class__,self.instance_dict[i]))
-                
-                if i in kwargs:
-                    setattr(self,i,kwargs[i])
-                else:
-                    setattr(self,i,self.instance_dict[i].db_value())
+                if len(self.kwargs) > 0:
+                    if i in self.kwargs:
+                        setattr(self,i,self.kwargs[i])
+                    else:
+                        setattr(self,i,self.instance_dict[i].db_value())
 
                 inst_dict[i] = self.instance_dict[i]
         
         self.instance_dict = inst_dict
 
-        self.objects = self.DB_Objects(self)
+    
+    def objects(self):
+        self.__init__()
+        sub_obj = self.DB_Objects(self)
+        return sub_obj
        
 
     def create_table(self):
@@ -81,18 +90,26 @@ class DB_Model():
         query = "UPDATE {} SET ".format(self.model_name)
 
         for i in self.instance_dict:
-            query += "{} = '{}'".format(i,self.instance_dict[i].db_value())
+            if not i == "id":
+                self.__dict__[i+"_field"].field.kwargs["value"] = self.__dict__[i]
+                query += "{} = '{}'".format(i,self.instance_dict[i].db_value())
+                
+                if not list( self.instance_dict.keys() ).index(i) == len( self.instance_dict.keys() )-1:
+                    query += ", "
             
-            if not list( self.instance_dict.keys() ).index(i) == len( self.instance_dict.keys() )-1:
-                query += ", "
-            
-        query += " WHERE username = {}".format("'ali'")
+        query += " WHERE id = {}".format(self.id)
 
         def ct_wrapper(q,db_q):
             db_q.cursor.execute(q)
             db_q.connection.commit()
         
         Query.run(query,ct_wrapper)
+
+    def save(self):
+        if self.id:
+            self._update()
+        else:
+            self._insert()
     
     class DB_Objects:
         def __init__(self,p_cls):
@@ -106,13 +123,50 @@ class DB_Model():
                 db_q.cursor.execute(q)
                 q_set = db_q.cursor.fetchall()
                 if len(q_set) > 0:
-                    self.query_set = q_set
+                    for i in q_set:
+                        #generate new model classes for each record
+                        m = self.p_cls.__class__()
+                        m.kwargs = i
+                        m.set_kwargs_as_attr()
+                        self.p_cls.query_set.append(m)
                 else:
-                    self.query_set = []
+                    self.p_cls.query_set = []
             
             Query.run(query,ct_wrapper)
 
-            return self.query_set # will return a query set object here
+            return self.p_cls.query_set
+
+        def filter(self,**kwargs):
+            query = f"Select * From {self.p_cls.model_name} where "
+
+            for kw in kwargs:
+                query += f"{kw} = '{kwargs[kw]}' "
+
+                if not list( kwargs.keys() ).index(kw) == len( kwargs.keys() )-1:
+                    query += "and "
+
+            def ct_wrapper(q,db_q):
+                db_q.cursor.execute(q)
+                q_set = db_q.cursor.fetchall()
+                if len(q_set) > 0:
+                    for i in q_set:
+                        #generate new model classes for each record
+                        m = self.p_cls.__class__()
+                        m.kwargs = i
+                        m.set_kwargs_as_attr()
+                        self.p_cls.query_set.append(m)
+                else:
+                    self.p_cls.query_set = []
+            
+            Query.run(query,ct_wrapper)
+
+            return self.p_cls.query_set
+
+
+
+
+
+        
     
 
 
