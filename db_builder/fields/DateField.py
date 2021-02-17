3d8cf2335188ac5c397dd1166c98ff71ff9825b4
Swindler36 <furkanzerman@gmail.com>
2021-02-17 23:44:26 +0300

    adding date field

---
+++ db_builder/fields/DateField.py
@@ -0,0 +1,29 @@
+from ..db_fields import DB_Field
+from datetime import datetime,date
+
+
+class DateField(DB_Field):
+    def __init__(self,**kwargs):
+        self.kwargs = kwargs
+        self.field_name = ""
+        self.field_type = "varchar(64)"
+
+    def is_valid(self):
+        if self.get_kwarg("auto_now") == True:
+            return True    
+        elif type(self.get_kwarg("value")).__name__ == "datetime":
+            return True
+        return False
+
+    def db_value(self):
+        if self.is_valid():
+            if self.get_kwarg("auto_now") == True:     
+                now = datetime.now()
+                return date(now.year,now.month,now.day).isoformat()
+            else:
+                return self.get_kwarg("value")
+
+        return False
+
+    def get_value(self,value):
+        pass
